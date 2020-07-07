ece5a91f03236808f3713ecfb21b93c651d94a67
Kate Mills <kate.mills1124@gmail.com>
2020-07-06 18:04:48 -0700

    Forgot to remove (testing) background colors

--- src/components/globals/Heading.js
+++ src/components/globals/Heading.js
@@ -2,15 +2,13 @@ import React from 'react'
 import {screen} from '../../media-styles'
 import styled from 'styled-components'
 
-const Header  = ({childrenFirst, childrenLast, children,  text, num, className}) => {
+const Header  = ({children,  text, num, className}) => {
   return (
     <div className={className}>
-      {childrenFirst ? <div>{children}</div>: <></>}
       { num > 1 ? 
          <h2>{text}</h2>:
          <h1>{text}</h1>
       }
-      {childrenLast ? <div>{children}</div>: <></>}
     </div>
   )
 }
@@ -26,8 +24,11 @@ export default styled(Header)`
    `}
    ${screen.tablet.tablet`
       &{width: 100vw;}
+      background: orange;
    `}
   ${screen.desktop.desktop`
       &{width: 80vw;}
+      background: green;
   `}
 `
+
